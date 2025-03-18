65f08d67fa46a8f2855347936fb74735a29f2138
Rounak <macbookpro@Rounaks-MacBook-Pro.local>
2025-03-18 19:15:09 +0530

    Added main/hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,10 +1,3 @@
-//File: hello.cpp
 #include <iostream>
 using namespace std;
-
-int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
-  return 0;
-}
+int main() { cout << "Hello, Jenkins Pipeline!" << endl; return 0; }
